a8c074cc97c77508c8d672aea047150ab1b5427c
Ansel Bridgewater <iamansel@gmail.com>
2019-04-06 13:16:13 -0700

    Created next block component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './index.css';
 
 import GridBoard from './components/grid-board'
+import NextBlock from './components/next-block'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <h1 className="App-title">Tetris Redux</h1>
         </header>
         <GridBoard />
+        <NextBlock />
       </div>
     );
   }
