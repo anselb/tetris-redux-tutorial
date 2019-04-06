f42a702a9bdea3f3fda68384b8c3bb3bbdba2709
Ansel Bridgewater <iamansel@gmail.com>
2019-04-06 16:40:30 -0700

    Added some utils and shape patterns

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,163 @@
 export const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+// gridDefault returns the default grid
+export const gridDefault = () => {
+  const rows = 18
+  const cols = 10
+  const array = []
+
+  for(let row = 0; row < rows; row++) {
+    const rowArr = []
+    for(let col = 0; col < cols; col++) {
+      rowArr.push(0)
+    }
+    array.push(rowArr)
+  }
+
+  return array
+}
+
+// randomShape returns index of random shape from 1 to the number of items in `shapes`
+export const randomShape = () => {
+  // skip the first item, which is an empty shape
+  return random(1, shapes.length - 1)
+}
+
+// defaultState returns the default state for the game
+export const defaultState = () => {
+  return {
+    // create an empty grid
+    grid: gridDefault(),
+    // get a new random shape
+    shape: randomShape(),
+    // set rotation of the shape to 0
+    rotation: 0,
+    // set the 'x' position of the shape to 5 and y to -4, which puts the shape
+    // in the center of the grid, above the top
+    x: 5,
+    y: -4,
+    // set the index of the next shape to a new random shape
+    nextShape: randomShape(),
+    // set game to currently running
+    isRunning: true,
+    // set the score to 0
+    score: 0,
+    // set the default speed
+    speed: 1000,
+    // set game to not over
+    gameOver: false
+  }
+}
+
+// shapes defines block shapes and their rotations as arrays.
+export const shapes = [
+  // none
+  [[[0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]]],
+
+  // I
+  [[[0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]]],
+
+  // T
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // L
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [1,0,0,0],
+    [0,0,0,0]],
+
+   [[1,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,0,1,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]]],
+
+  // J
+  [[[1,0,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,1,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,0,0,0],
+    [1,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [1,1,0,0],
+    [0,0,0,0]]],
+
+  // Z
+  [[[0,0,0,0],
+    [1,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+
+   [[0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // S
+  [[[0,0,0,0],
+    [0,1,1,0],
+    [1,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]]],
+
+  // O
+  [[[0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]]]
+]
