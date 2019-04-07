302259acdef1936a7ddf15d3f53fbc09796a53ab
Ansel Bridgewater <iamansel@gmail.com>
2019-04-07 14:49:32 -0700

    Connected message popup to redux

--- src/components/message-popup.js
+++ src/components/message-popup.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class MessagePopup extends Component {
 
   render() {
+    const { isRunning, gameOver } = this.props
+
     return (
-      <div className='message-popup'>
-        <h1>Message Title</h1>
-        <p>Message info...</p>
+      <div className={`message-popup ${isRunning && !gameOver ? 'hidden' : ''}`}>
+        <h1>{ gameOver ? 'Game Over' : 'Paused'}</h1>
+        <p>{ gameOver ? 'Click "Restart" to try again' : 'Click "Play" to resume'}</p>
       </div>
     )
   }
 }
 
-export default MessagePopup
+const mapStateToProps = (state) => {
+  return {
+    isRunning: state.game.isRunning,
+    gameOver: state.game.gameOver
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(MessagePopup)
